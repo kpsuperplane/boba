fcd48f317830234211012032384e2ef7867578b6
Kevin Pei <kevin-pei@hotmail.com>
2018-02-03 21:17:52 -0500

    docs

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@ extern crate pancurses;
 use pancurses::{initscr, endwin};
 
 fn main() {
+
   let window = initscr();
+
+  // assume 4x4 box
   let start = window.get_max_x() / 2 - 2;
   let col = window.get_max_y() / 2 - 2;
+
+  // draw a drink
   window.mvprintw(col - 1, start, "  / ");
   window.mvprintw(col, start, "====");
   window.mvprintw(col + 1, start, "|~~|");
@@ -13,6 +18,10 @@ fn main() {
   window.mvprintw(col + 3, start, "`--`");
   window.mv(0, 0);
   window.refresh();
+
+  // wait
   window.getch();
+
+  // slurp
   endwin();
 }
